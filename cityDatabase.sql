9e51a8622d2089c490c1c5518ef9432b6a6cf697
Cameron Kuchta <kuchtact@beloit.edu>
2019-02-13 08:14:13 -0600

    Fix database creation.

--- cityDatabase.sql
+++ cityDatabase.sql
@@ -1,21 +1,25 @@
+CREATE DATABASE IF NOT EXISTS city_project;
+
+USE city_project;
+
+CREATE TABLE owner_parcel (
+  owner_id SMALLINT UNSIGNED PRIMARY KEY,
+  owner_zipcode SMALLINT UNSIGNED
+);
+
 CREATE TABLE parcel (
-  parcel_id SMALLINT UNSIGNED PRIMARY KEY,
-  wner_id SMALLINT UNSIGNED,
+  parcel_id VARCHAR(20) PRIMARY KEY,
+  owner_id SMALLINT UNSIGNED,
   FOREIGN KEY(owner_id) REFERENCES owner_parcel(owner_id),
   zipcode SMALLINT UNSIGNED,
   rental BOOLEAN,
   census_tracts NUMERIC(10,1)
 );
 
-CREATE TABLE owner_parcel (
-	owner_id SMALLINT UNSIGNED PRIMARY KEY,
-	owner_zipcode SMALLINT UNSIGNED
-);
-
 CREATE TABLE violation (
-  id SMALLINT UNSIGNED PRIMARY KEY,
-  parcel_id VARCHAR(20),
-  FOREIGN KEY(parcel_id) REFERENCES parcel(parcel_id),
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  parcel_id VARCHAR(20) NOT NULL,
+  FOREIGN KEY (parcel_id) REFERENCES parcel(parcel_id),
   violation_code SMALLINT UNSIGNED,
   violation_id SMALLINT UNSIGNED
 );
